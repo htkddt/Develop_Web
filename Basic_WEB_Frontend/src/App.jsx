add1a7c26c62df06415910b22cdd08635149d9a0
htkddt <huynhtuankietddt@gmail.com>
2024-06-01 21:01:51 +0700

    Update

--- Basic_WEB_Frontend/src/App.jsx
+++ Basic_WEB_Frontend/src/App.jsx
@@ -8,57 +8,18 @@
 // import HomeIcon from '@mui/icons-material/Home'
 // import { pink } from '@mui/material/colors'
 // import Typography from '@mui/material/Typography'
-import { useColorScheme } from '@mui/material/styles'
+// import { useColorScheme } from '@mui/material/styles'
 // import useMediaQuery from '@mui/material/useMediaQuery'
 
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Box from '@mui/material/Box'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Container from '@mui/material/Container'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-  const handleChange = (event) => {
-    const selectedMode = event.target.value
-    setMode(selectedMode)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-            <LightModeIcon fontSize="small" /> Light
-          </div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            {/* gap = 1 ứng với giá trị là 8px */}
-            <DarkModeOutlinedIcon fontSize="small" /> Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-            {/* gap = 2 ứng với giá trị là 16px */}
-            <SettingsBrightnessIcon fontSize="small" /> System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+// import InputLabel from '@mui/material/InputLabel'
+// import MenuItem from '@mui/material/MenuItem'
+// import FormControl from '@mui/material/FormControl'
+// import Select from '@mui/material/Select'
+// import Box from '@mui/material/Box'
+// import LightModeIcon from '@mui/icons-material/LightMode'
+// import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+// import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+// import Container from '@mui/material/Container'
 
 // function ModeToggle() {
 //   const { mode, setMode } = useColorScheme()
@@ -92,6 +53,8 @@ function ModeSelect() {
 // import TabIndicator from '@mui/material/Tabs/TabIndicator';
 // //                                           ^^^^^^^^^^^^ 3rd level
 
+import Board from './pages/Boards/_id'
+// Update
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -100,78 +63,83 @@ function App() {
   // console.log(count)
 
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.layoutCustom.appBar_height,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        App Bar
-        <ModeSelect />
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.layoutCustom.boardBar_height,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Bar
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        height: (theme) => `calc(100vh - ${theme.layoutCustom.appBar_height} - ${theme.layoutCustom.boardBar_height})`,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Contents
-      </Box>
+    <>
+      {/* React Router Dom /Boards/Board/(board_id) */}
+      {/* Board Details */}
+      <Board />
+    </>
+  // <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+  //   <Box sx={{
+  //     backgroundColor: 'primary.light',
+  //     width: '100%',
+  //     height: (theme) => theme.layoutCustom.appBar_height,
+  //     display: 'flex',
+  //     alignItems: 'center'
+  //   }}>
+  //     App Bar
+  //     <ModeSelect />
+  //   </Box>
+  //   <Box sx={{
+  //     backgroundColor: 'primary.dark',
+  //     width: '100%',
+  //     height: (theme) => theme.layoutCustom.boardBar_height,
+  //     display: 'flex',
+  //     alignItems: 'center'
+  //   }}>
+  //     Board Bar
+  //   </Box>
+  //   <Box sx={{
+  //     backgroundColor: 'primary.main',
+  //     width: '100%',
+  //     height: (theme) => `calc(100vh - ${theme.layoutCustom.appBar_height} - ${theme.layoutCustom.boardBar_height})`,
+  //     display: 'flex',
+  //     alignItems: 'center'
+  //   }}>
+  //     Board Contents
+  //   </Box>
 
-      {/* <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+  //   {/* <div>
+  //     <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+  //       <img src={viteLogo} className="logo" alt="Vite logo" />
+  //     </a>
+  //     <a href="https://react.dev" rel="noreferrer" target="_blank">
+  //       <img src={reactLogo} className="logo react" alt="React logo" />
+  //     </a>
+  //   </div>
+  //   <h1>Vite + React</h1>
+  //   <div className="card">
+  //     <button onClick={() => setCount((count) => count + 1)}>
+  //       count is {count}
+  //     </button>
+  //     <p>
+  //       Edit <code>src/App.jsx</code> and save to test HMR
+  //     </p>
+  //   </div>
+  //   <p className="read-the-docs">
+  //     Click on the Vite and React logos to learn more
+  //   </p> */}
 
-      {/* <hr />
-      <ModeToggle />
-      <hr />
-      <div>huynhtuankietddt</div>
+  //   {/* <hr />
+  //   <ModeToggle />
+  //   <hr />
+  //   <div>huynhtuankietddt</div>
 
-      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+  //   <Typography variant="body2" color="text.secondary">Test Typography</Typography>
 
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color="success">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <br />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} /> */}
-    </Container>
+  //   <Button variant="text">Text</Button>
+  //   <Button variant="contained" color="success">Contained</Button>
+  //   <Button variant="outlined">Outlined</Button>
+  //   <br />
+  //   <AccessAlarmIcon />
+  //   <ThreeDRotation />
+  //   <br />
+  //   <HomeIcon color="primary" />
+  //   <HomeIcon color="secondary" />
+  //   <HomeIcon color="success" />
+  //   <HomeIcon color="action" />
+  //   <HomeIcon color="disabled" />
+  //   <HomeIcon sx={{ color: pink[500] }} /> */}
+  // </Container>
   )
 }
 
