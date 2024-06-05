c61b288956c6209afbd36b5235df86dcdb77cd71
htkddt <huynhtuankietddt@gmail.com>
2024-06-05 21:44:20 +0700

    Board Bar Design #1

--- Basic_WEB_Frontend/src/pages/Boards/BoardBar/index.jsx
+++ Basic_WEB_Frontend/src/pages/Boards/BoardBar/index.jsx
@@ -10,7 +10,45 @@ function BoardBar() {
       alignItems: 'center',
       color: 'black'
     }}>
-      Board Bar
+      <Box sx={{
+        backgroundColor: 'yellowgreen',
+        width: '50%',
+        height: (theme) => theme.layoutCustom.boardBar_height,
+        display: 'flex',
+        alignItems: 'center', // Canh giữa nội dung theo chiều dọc
+        justifyContent: 'center', // Canh giữa nội dung theo chiều ngang
+        color: 'black',
+        fontSize: '35px'
+      }}>
+        MAIN HEADER
+      </Box>
+      <Box sx={{
+        width: '50%',
+        height: (theme) => theme.layoutCustom.boardBar_height
+      }}>
+        <Box sx={{
+          backgroundColor: 'orange',
+          width: (theme) => theme.layoutCustom.layoutwidth,
+          height: '30px',
+          display: 'flex',
+          alignItems: 'center', // Canh giữa nội dung theo chiều dọc
+          justifyContent: 'center', // Canh giữa nội dung theo chiều ngang
+          color: 'black'
+        }}>
+          Header Title 1
+        </Box>
+        <Box sx={{
+          backgroundColor: 'orange',
+          width: (theme) => theme.layoutCustom.layoutwidth,
+          height: '30px',
+          display: 'flex',
+          alignItems: 'center', // Canh giữa nội dung theo chiều dọc
+          justifyContent: 'center', // Canh giữa nội dung theo chiều ngang
+          color: 'black'
+        }}>
+          Header Title 2
+        </Box>
+      </Box>
     </Box>
   )
 }
